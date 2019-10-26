9c7a99242e15dd0a97a8fed599a186b7d7c56dc4
Mario Mannone <56471098+mario-mannone-peano-4a@users.noreply.github.com>
2019-10-26 09:59:05 +0200

    Rimosso variabili superflue

---
+++ Calcoloeareadiunperimetromodifica1.java
@@ -0,0 +1,30 @@
+package Calcoloeareadiunperimetromodifica1;
+
+        import java.util.Scanner;
+public class Calcoloeareadiunperimetromodifica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int b, h;
+        Scanner tastiera=new Scanner(System.in);
+        
+        System.out.println("Inserisci la base: ");
+        b=tastiera.nextInt();
+        System.out.println("Inserisci l'altezza: ");
+        h=tastiera.nextInt();
+        
+        System.out.println("Area: "+ calcolo(b, h, 'a'));
+        System.out.println("Perimetro: "+calcolo(b, h, 'p'));
+    }
+    
+    public static int calcolo(int b, int h, char c){
+        switch(c){
+            case 'p':return (b+h)*2;
+            case 'a': return b*h;
+            default:return 0;
+            
+        }
+    }  
+}
